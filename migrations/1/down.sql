6788f26ec04bb26a313ce8b3bdf10b2e56d862f5
Person333333 <107582257+Person333333@users.noreply.github.com>
2025-11-04 23:40:27 -0800

    Did stuff

--- migrations/1/down.sql
+++
@@ -1,7 +0,0 @@
-
-DROP INDEX idx_submissions_status;
-DROP TABLE resource_submissions;
-DROP INDEX idx_resources_is_approved;
-DROP INDEX idx_resources_is_featured;
-DROP INDEX idx_resources_category;
-DROP TABLE resources;
